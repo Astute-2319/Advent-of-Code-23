70d1f07240696f00f8889582fdf8cee2fda05c93
Alex Stute <stute5000@gmail.com>
2024-02-26 22:08:34 -0500

    working more on Day 3 puzzle 1

--- Day 3/Puzzle1.py
+++ Day 3/Puzzle1.py
@@ -11,6 +11,11 @@ line1 = []
 line2 = []
 line3 = []
 
+# create a dictionary to store where symbols are for each line
+# key will be the line num
+# data will be a list of symbol locations
+symbols = {'line_1': [], 'line_2': [], 'line_3': []}
+
 # for each line
 for x in inputFile:
     # push all the other lines "down"
@@ -23,27 +28,40 @@ for x in inputFile:
     # save the line to the "highest" of the three lists
     line3.clear()
     for i in x:
-        line3.append(i)
+        if i != '\n':
+            line3.append(i)
 
-    # start reading numbers once each line list is filled
+    # start reading number and symbol locations once each line list is filled
     if line1 != []:
         line_pos = 0
         # iterate through line 1, getting locations of symbols and numbers
         print("Line 1")
         for item in line1:
-            # item is a symbol
+            # if item is a symbol
             if not re.search("[0-9.\n]", item):
-                print(item)
+                # save the position of the symbol
+                symbols["line_1"].append(line_pos)
+            line_pos += 1
+
+        line_pos = 0
+        # iterate through line 2, getting locations of symbols and numbers
         print("Line 2")
         for item in line2:
-            # item is a symbol
+            # if item is a symbol
             if not re.search("[0-9.\n]", item):
-                print(item)
+                # save the position of the symbol
+                symbols["line_2"].append(line_pos)
+            line_pos += 1
+
+        line_pos = 0
+        # iterate through line 3, getting locations of symbols and numbers
         print("Line 3")
         for item in line3:
-            # item is a symbol
+            # if item is a symbol
             if not re.search("[0-9.\n]", item):
-                print(item)
+                # save the position of the symbol
+                symbols["line_3"].append(line_pos)
+            line_pos += 1
         
 
     
